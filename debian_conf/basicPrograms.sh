112da5b7d1066f24f420060981855470c7bc0f51
Amadeo Morán <amadeo.moran@gmail.com>
2020-04-06 13:23:32 +0200

    [UPD]

--- debian_conf/basicPrograms.sh
+++ debian_conf/basicPrograms.sh
@@ -5,13 +5,6 @@ echo "#########################################################"
 echo "After installation script to set up and install programs"
 echo "#########################################################"
 
-# Give root permisons to the current user 
-# from https://devconnected.com/how-to-add-a-user-to-sudoers-on-debian-10-buster/
-su
-user=whoami
-sudo usrmod -a -G sudo $user
-exit
-
 # Verify 
 su - $user
 echo groups
