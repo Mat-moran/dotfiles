194f21b53e81ea56382745dbe7a9cdce413612fe
Amadeo Morán <amadeo.moran@gmail.com>
2020-04-06 13:26:17 +0200

    [UPD]

--- debian_conf/sudoPrivileges.sh
+++ debian_conf/sudoPrivileges.sh
@@ -4,3 +4,8 @@ su
 user=whoami
 sudo usrmod -a -G sudo $user
 exit
+
+
+# If wireless connection is required it is neccessary to be able to connect to non-free repository 
+# After install iwlwifi package for intel wifi controller
+# https://wiki.debian.org/iwlwifi
