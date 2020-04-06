ffcfb9a7fc5f62aa52c86b6345abaeb4b0475b5e
Amadeo Morán <amadeo.moran@gmail.com>
2020-04-06 13:51:28 +0200

    [UPD]

--- debian_conf/basicPrograms.sh
+++ debian_conf/basicPrograms.sh
@@ -5,10 +5,6 @@ echo "#########################################################"
 echo "After installation script to set up and install programs"
 echo "#########################################################"
 
-# Verify 
-su - $user
-echo groups
-
 sudo apt-get update
 sudo apt-get install fish
 sudo apt-get install neovim
